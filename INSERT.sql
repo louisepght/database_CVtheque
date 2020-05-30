624072a7fa0a7ee0fcbb911776ca94488a4c2f3d
Louise Poughet <louise.poughet@etu.utc.fr>
2020-05-30 13:01:54 +0200

    Update INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -1,20 +1,20 @@
-INSERT INTO()
-VALUES();
+INSERT INTO info_complementaire
+VALUES('1234','oui','non');
 
-INSERT INTO()
-VALUES();
+INSERT INTO formation
+VALUES('Ingénieur informatique','UTC','master','01/09/2020','30/06/2025','France','compiègne');
 
-INSERT INTO()
-VALUES();
+INSERT INTO competence
+VALUES('communication');
 
-INSERT INTO()
-VALUES();
+INSERT INTO langue
+VALUES('anglais');
 
-INSERT INTO()
-VALUES();
+INSERT INTO association
+VALUES('toit pour le nepal','2','resp com');
 
-INSERT INTO()
-VALUES();
+INSERT INTO CV
+VALUES('0101','rechrche emploi','francais','désactivé','01/05/2019','03/05/2019','1234');
 
 INSERT INTO()
 VALUES();
