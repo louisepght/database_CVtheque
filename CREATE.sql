61a56094b1c8a63654bc64bb7f778401f77c9902
Louise Poughet <louise.poughet@etu.utc.fr>
2020-05-30 12:36:14 +0200

    Update CREATE.sql debeug

--- CREATE.sql
+++ CREATE.sql
@@ -1,86 +1,3 @@
-
-	
-CREATE TABLE candidat(
-    identifiant VARCHAR,
-    mot_de_passe VARCHAR NOT NULL,
-    nom VARCHAR NOT NULL,
-    prenom VARCHAR NOT NULL,
-    e_mail  VARCHAR NOT NULL,
-    cv INTEGER NOT NULL REFERENCES CV(IDCV),
-    UNIQUE(cv),
-    PRIMARY KEY(identifiant)
-);
-
-
-CREATE TABLE CV(
-    IDCV INTEGER,
-    titre VARCHAR NOT NULL,
-    langue VARCHAR NOT NULL,
-    statut VARCHAR NOT NULL,
-    date_crea DATE NOT NULL,
-    date_modif DATE NOT NULL,
-    info_CV INTEGER REFERENCES info_complementaire(IDINFO),
-    PRIMARY KEY(IDCV),
-    CHECK(langue IN ('francais', 'anglais')),
-    CHECK(statut IN ('activé','confidentiel','désactivé'))
-);
-
-CREATE TABLE referant(
-    nom_entreprise VARCHAR,
-    nom VARCHAR NOT NULL,
-    prenom VARCHAR NOT NULL,
-    candidat INTEGER NOT NULL,
-    PRIMARY KEY(nom_entreprise),
-    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
-    UNIQUE(candidat)
-);
-
-CREATE TABLE publication(
-    ISBN INTEGER,
-    titre VARCHAR NOT NULL,
-    date DATETIME NOT NULL,
-    contenu VARCHAR NOT NULL,
-    candidat INTEGER NOT NULL,
-    PRIMARY KEY(ISBN),
-    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
-    UNIQUE(candidat),
-    CHECK (LENGTH(ISBN)=10 OR LENGTH(ISBN)=13)
-);
-
-CREATE TABLE web(
-    URL VARCHAR,
-    type_web VARCHAR NOT NULL,
-    candidat INTEGER NOT NULL,
-    PRIMARY KEY(URL),
-    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
-    UNIQUE(candidat),
-    CHECK (type_web IN ('personnelle','professionnelle')),
-    CHECK (SUBSTR(URL,1,8) ='https://')
-);
-
-CREATE TABLE telephone(
-    numero VARCHAR(10),   
-    type_tel VARCHAR,
-    candidat INTEGER,
-    PRIMARY KEY(numero),
-    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
-    UNIQUE(candidat),
-    CHECK(type_tel IN ('port_perso','port_pro','fixe_perso','fixe_pro'))
-);
-
-CREATE TABLE experience(
-    nom_entreprise VARCHAR,
-    titre_poste VARCHAR NOT NULL,
-    date_debut DATE NOT NULL,
-    date_fin DATE NOT NULL,
-    fonction VARCHAR NOT NULL,
-    secteur_act VARCHAR NOT NULL,
-    cv INTEGER NOT NULL,
-    PRIMARY KEY(nom_entreprise),
-    FOREIGN KEY(cv) REFERENCES CV(IDCV),
-    UNIQUE (cv)
-);
-
 CREATE TABLE info_complementaire(
     IDINFO INTEGER,
     permis VARCHAR,
@@ -89,8 +6,8 @@ CREATE TABLE info_complementaire(
 );
 
 CREATE TABLE formation(
-    titre VARCHAR,
-    etablissement VARCHAR,
+    titre VARCHAR UNIQUE NOT NULL,
+    etablissement VARCHAR UNIQUE NOT NULL,
     type_formation VARCHAR NOT NULL,
     date_debut DATE NOT NULL,
     date_fin DATE NOT NULL,
@@ -115,10 +32,91 @@ CREATE TABLE association(
     poste VARCHAR,
     PRIMARY KEY(nom)
 );
+CREATE TABLE CV(
+    IDCV INTEGER,
+    titre VARCHAR NOT NULL,
+    langue VARCHAR NOT NULL,
+    statut VARCHAR NOT NULL,
+    date_crea DATE NOT NULL,
+    date_modif DATE NOT NULL,
+    info_CV INTEGER REFERENCES info_complementaire(IDINFO),
+    PRIMARY KEY(IDCV),
+    CHECK(langue IN ('francais', 'anglais')),
+    CHECK(statut IN ('activé','confidentiel','désactivé'))
+);
+
+CREATE TABLE candidat(
+    identifiant VARCHAR,
+    mot_de_passe VARCHAR NOT NULL,
+    nom VARCHAR NOT NULL,
+    prenom VARCHAR NOT NULL,
+    e_mail  VARCHAR NOT NULL,
+    cv INTEGER NOT NULL REFERENCES CV(IDCV),
+    UNIQUE(cv),
+    PRIMARY KEY(identifiant)
+);
+
+
+CREATE TABLE referant(
+    nom_entreprise VARCHAR,
+    nom VARCHAR NOT NULL,
+    prenom VARCHAR NOT NULL,
+    candidat VARCHAR NOT NULL,
+    PRIMARY KEY(nom_entreprise),
+    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
+    UNIQUE(candidat)
+);
+
+CREATE TABLE publication(
+    ISBN INTEGER,
+    titre VARCHAR NOT NULL,
+    date DATE NOT NULL,
+    contenu VARCHAR NOT NULL,
+    candidat VARCHAR NOT NULL,
+    PRIMARY KEY(ISBN),
+    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
+    UNIQUE(candidat),
+    CHECK ((ISBN)=10 OR (ISBN)=13)
+);
+
+CREATE TABLE web(
+    URL VARCHAR,
+    type_web VARCHAR NOT NULL,
+    candidat VARCHAR NOT NULL,
+    PRIMARY KEY(URL),
+    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
+    UNIQUE(candidat),
+    CHECK (type_web IN ('personnelle','professionnelle')),
+    CHECK (SUBSTR(URL,1,8) ='https://')
+);
+
+CREATE TABLE telephone(
+    numero VARCHAR(10),   
+    type_tel VARCHAR,
+    candidat VARCHAR,
+    PRIMARY KEY(numero),
+    FOREIGN KEY(candidat) REFERENCES candidat(identifiant),
+    UNIQUE(candidat),
+    CHECK(type_tel IN ('port_perso','port_pro','fixe_perso','fixe_pro'))
+);
+
+CREATE TABLE experience(
+    nom_entreprise VARCHAR,
+    titre_poste VARCHAR NOT NULL,
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+    fonction VARCHAR NOT NULL,
+    secteur_act VARCHAR NOT NULL,
+    cv INTEGER NOT NULL,
+    PRIMARY KEY(nom_entreprise),
+    FOREIGN KEY(cv) REFERENCES CV(IDCV),
+    UNIQUE (cv)
+);
+
 
 CREATE TABLE vie_associative(
     association VARCHAR,
-    candidat INTEGER,
+    candidat VARCHAR,
     FOREIGN KEY (association) REFERENCES association(nom),
     FOREIGN KEY(candidat) REFERENCES candidat(identifiant)
 );
@@ -139,12 +137,12 @@ CREATE TABLE asso_comp(
 );
 
 CREATE TABLE asso_formation(
-    formation VARCHAR,
-    cv INTEGER,
-    FOREIGN KEY (formation) REFERENCES formation,
+    formation VARCHAR NOT NULL,
+    cv INTEGER NOT NULL,
+    UNIQUE(formation,cv),
+    FOREIGN KEY (formation) REFERENCES formation(titre),
     FOREIGN KEY (cv) REFERENCES CV(IDCV)
 );
-
 
 
 
