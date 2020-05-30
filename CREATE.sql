451f1706d59c3099173e7adcd41786a58e18c3f4
Louise Poughet <louise.poughet@etu.utc.fr>
2020-05-30 11:37:15 +0200

    Update CREATE.sql

--- CREATE.sql
+++ CREATE.sql
@@ -1,15 +1,4 @@
--- Database: cvtheque
 
--- DROP DATABASE cvtheque;
-
-CREATE DATABASE cvtheque
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'French_France.1252'
-    LC_CTYPE = 'French_France.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
 	
 CREATE TABLE candidat(
     identifiant VARCHAR,
