5633dca1100f2927d1ac12f9a576cda87c508d73
Louise Poughet <louise.poughet@etu.utc.fr>
2020-05-30 16:26:59 +0200

    Update INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -1,54 +1,29 @@
-INSERT INTO info_complementaire
-VALUES('1234','oui','non');
+INSERT INTO candidat
+VALUES('louiselog','mdplouise','poughet','louise','louise.poughet@gmail.com','0101');
 
-INSERT INTO formation
-VALUES('Ingénieur informatique','UTC','master','01/09/2020','30/06/2025','France','compiègne');
 
-INSERT INTO competence
-VALUES('communication');
+INSERT INTO referant
+VALUES('AMIS','francois','duval','louiselog');
 
-INSERT INTO langue
-VALUES('anglais');
 
-INSERT INTO association
-VALUES('toit pour le nepal','2','resp com');
+INSERT INTO web
+VALUES('https://mywebsite','personnelle','louiselog');
 
-INSERT INTO CV
-VALUES('0101','rechrche emploi','francais','désactivé','01/05/2019','03/05/2019','1234');
+INSERT INTO telephone
+VALUES('0602030405','port_perso','louiselog');
 
-INSERT INTO()
-VALUES();
+INSERT INTO experience
+VALUES('AMIS','stage opérateur','01/02/2019','30/03/2019','ouvrier','siderurgie','0101');
 
-INSERT INTO()
-VALUES();
+INSERT INTO vie_associative
+VALUES('toit pour le nepal','louiselog');
 
-INSERT INTO()
-VALUES();
+INSERT INTO niveau
+VALUES('anglais','0101 ','courant');
 
-INSERT INTO()
-VALUES();
+INSERT INTO asso_comp
+VALUES('communication','0101');
 
-INSERT INTO()
-VALUES();
-
-INSERT INTO()
-VALUES();
-
-INSERT INTO()
-VALUES();
-
-INSERT INTO()
-VALUES();
-
-INSERT INTO()
-VALUES();
-
-INSERT INTO()
-VALUES();
-
-INSERT INTO()
-VALUES();
-
-INSERT INTO()
-VALUES();
+INSERT INTO asso_formation
+VALUES('Ingénieur informatique','0101');
 
