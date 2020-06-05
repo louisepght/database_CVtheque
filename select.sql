8829dcc14915aefaac62c5eda2dd3e706b2bc322
Louise Poughet <louise.poughet@etu.utc.fr>
2020-06-05 15:46:07 +0200

    Update select.sql

--- select.sql
+++ select.sql
@@ -10,7 +10,7 @@ SELECT COUNT (*)
 FROM formation 
 WHERE type_formation='BTS'
 
-//sélection de la compétence commmunication -> à remplacer par base de données
+--sélection de la compétence commmunication -> à remplacer par base de données
 SELECT (CV.IDCV)
 FROM asso_comp,CV,competence
 WHERE competence.nom = 'communication' AND asso_comp.competence = competence.nom AND asso_comp.cv = CV.IDCV
